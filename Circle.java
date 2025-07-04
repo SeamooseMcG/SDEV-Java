b932f277dc67a4a0b5f4d9f2b4d4dafdc88aee23
SeamooseMcG <seamusfilkins@gmail.com>
2025-07-04 00:32:36 -0500

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,52 @@
+public class Circle extends GeometricObject implements Comparable<Circle> {
+    private double radius;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * radius * Math.PI;
+    }
+
+    public double getDiameter() {
+        return 2 * radius;
+    }
+
+    public void printCircle() {
+        System.out.println("The circle is created " + getDateCreated() +
+                           " and the radius is " + radius);
+    }
+
+    // Implement Comparable interface
+    @Override
+    public int compareTo(Circle other) {
+        return Double.compare(this.radius, other.radius);
+    }
+
+    // Override equals method from Object
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Circle)) return false;
+        Circle other = (Circle) obj;
+        return Double.compare(this.radius, other.radius) == 0;
+    }
+}
