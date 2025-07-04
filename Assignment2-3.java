58deed8f445282c4e1de7cfe0b6270a5181091a1
SeamooseMcG <seamusfilkins@gmail.com>
2025-07-04 00:18:35 -0500

    Add files via upload

---
+++ Assignment2-3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class BinaryFormatException extends Exception {
+    public BinaryFormatException(String message) {
+        super(message);
+    }
+}
+
+public class BinaryConverter {
+
+    public static int bin2Dec(String binaryString) throws BinaryFormatException {
+        for (int i = 0; i < binaryString.length(); i++) {
+            char c = binaryString.charAt(i);
+            if (c != '0' && c != '1') {
+                throw new BinaryFormatException("Invalid binary string: " + binaryString);
+            }
+        }
+
+        return Integer.parseInt(binaryString, 2);
+    }
+}
+
